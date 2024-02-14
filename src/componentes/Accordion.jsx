3583a8751feb49d90e642e794194ed635d40cf5f
Mario <mariobl90@educastur.es>
2024-02-14 20:09:10 +0100

    Comentarios Añadidos

--- src/componentes/Accordion.jsx
+++ src/componentes/Accordion.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
 import { AccordionItem } from './AccordionItem';
-
+/*Componete en el cual se agrupan los AccordionItem*/
 export const Accordion = ({ items }) => {
+    /*Un hook para poder saber si el AccordionItem esta abierto o no(no lo esta por defecto) */
     const [itemActivo, setItemActivo] = useState(null);
     const handleItemClick = (question) => {
+        /*Revisa que esten cerrados y sino lo abre */
         setItemActivo((itemActivo) => {
             if (itemActivo === question) {
                 return 'No es igual';
@@ -13,7 +15,11 @@ export const Accordion = ({ items }) => {
             }
             });
         };
-    return (
+        /*question es pregunta que viene de el json*/
+        /*answer es la pregunta*/
+        /*isActive compara la pregunta del item que esta activo con la de la pregunta que acaba de entrar al clickar*/
+        /*on click es lo que sucede cuando se da click que llama a la funcion de arriba que compara a ver si es igual o no */
+    return ( 
     <ul className="accordion">
     {items.map((item) => (
         <AccordionItem
@@ -21,7 +27,7 @@ export const Accordion = ({ items }) => {
             question={item.pregunta}
             answer={item.respuesta}
             isActive={itemActivo === item.pregunta}
-            onClick={handleItemClick}
+            haceClick={handleItemClick}
         />
     ))}
   </ul>
--- src/componentes/AccordionItem.jsx
+++ src/componentes/AccordionItem.jsx
@@ -1,10 +1,12 @@
 import '../estilos/index.css';
 
 // eslint-disable-next-line react/prop-types
-export const AccordionItem = ({ question, answer, isActive, onClick }) => {
+export const AccordionItem = ({ question, answer, isActive, haceClick }) => {
+    /*Esta funcion es la funcion interna de este componente que llama a la funcion de comparar del componente de Accordion, cada vez que hay un click*/
     const handleClick = () => {
-    onClick(question);
+        haceClick(question);
     };
+
     return (
     <li className={`accordion_item ${isActive ? 'active' : ''}`}>
         <button className="button" onClick={handleClick}>
@@ -12,6 +14,7 @@ export const AccordionItem = ({ question, answer, isActive, onClick }) => {
             <span className="control">{isActive ? '--' : '+'}</span>
         </button>
         <div className={`answer_wrapper ${isActive ? 'open' : ''}`}>
+            {/*Esto es un poco rarete, consulte un libro, por que no encontraba la forma de hacer que cuando estuviera activo pintara el mensaje, se que no dimos nada parecido en clase*/}
             <div className="answer">{isActive && <p>{answer}</p>}</div>
         </div>
         </li>
